dde65fe3dcd80135c9966a4780c4e35562686f97
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2025-09-04 10:23:08 +0530

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,13 @@
+package Marvellous;
+
+public class Circle 
+{
+    public float CircleArea(float R)
+    {
+        return 3.14f * R * R;
+    }
+    
+}
+
+//javac -d . Circle.java
+//Don't executr this code directly
