daaf29d3ea9760e8231356f3fc9f8b0f0371d373
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2025-09-06 11:02:51 +0530

    Create FinalDemo2.java

---
+++ FinalDemo2.java
@@ -0,0 +1,29 @@
+
+class Base 
+{
+    final public void Fun()
+    public void Fun()
+    {}
+    public void Gun()
+    {}
+
+}
+
+class Derived extends Base
+{
+    public void Fun()       //Error
+    {}
+    public void Gun()
+    {}
+
+
+}
+
+class FinalDemo2 
+{
+    public static void main(String A[])
+    {
+
+    }
+    
+}
