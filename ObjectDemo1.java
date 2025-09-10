09d983d4f093353c8785512b16caab2d7c6841d8
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2025-09-10 21:20:12 +0530

    Create ObjectDemo1.java

---
+++ ObjectDemo1.java
@@ -0,0 +1,17 @@
+
+class Demo
+{
+
+}
+
+Class ObjectDemo1
+{
+    public static void main(String Arr[])
+    {
+        Demo obj = new Demo();
+
+        System.out.println(obj.getClass());
+    }
+
+    
+}
