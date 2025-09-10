a297ce5cf83175f8ebd2e48a4fdb9ecb8d8f406e
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2025-09-10 21:19:39 +0530

    Create ObjectDemo2.java

---
+++ ObjectDemo2.java
@@ -0,0 +1,15 @@
+
+class ObjectDemo2
+{
+    public static void main(String Arr[])
+    {
+        String s1 = "Marvellous";
+        String s2 = "PPA";
+        String s3 = "Marvellous";
+
+        System.out.printlm(s1.hashCode());
+        System.out.printlm(s2.hashCode());
+        System.out.printlm(s3.hashCode());
+    }
+  
+}
